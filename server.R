3f05d70f06ee6c0997359f41cd8f606c2cb70c28
DoomedNick <100158062+DoomedNick@users.noreply.github.com>
2025-04-16 12:15:11 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1966,12 +1966,10 @@ shinyServer(function(input, output, session) {
         
       }
       if (!is.na(sj) | dotaz_spiro == "YES") {
-        pandoc_path <- rmarkdown::find_pandoc()$dir
-        Sys.setenv(RSTUDIO_PANDOC = pandoc_path)
+        Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
         rmarkdown::render(paste(program_slozka, "/export_NEOTVIRAT.Rmd", sep = ""), params = list(dotaz_spiro = dotaz_spiro), output_file = paste(id, ".pdf", sep= ""), clean = T, quiet = F)
       } else {
-        pandoc_path <- rmarkdown::find_pandoc()$dir
-        Sys.setenv(RSTUDIO_PANDOC = pandoc_path)
+        Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
         rmarkdown::render(paste(program_slozka, "/export_NEOTVIRAT_wingate.Rmd", sep = ""), params = list(dotaz_spiro = dotaz_spiro), output_file = paste(id, ".pdf", sep= ""), clean = T, quiet = F)
       }
       
@@ -2536,8 +2534,8 @@ shinyServer(function(input, output, session) {
         png(paste0(program_slozka, "/p2.png"), width = 1500, height = 600)
         plot(combined_plot)
         dev.off()
-        pandoc_path <- rmarkdown::find_pandoc()$dir
-        Sys.setenv(RSTUDIO_PANDOC = pandoc_path)
+        
+        Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
         rmarkdown::render(paste(program_slozka, "/export_spiro_NEOTVIRAT.Rmd", sep = ""), output_file = paste(id, ".pdf", sep= ""), clean = T, quiet = F)
         
         fs::file_move(path = paste(program_slozka, "/", id, ".pdf", sep = ""), new_path = paste(wd, "/reporty/", id, ".pdf", sep = ""))
